22d1eb457c1fc274e289a62755efcdb7dadc784e
Daisy-Y-Cao <156784685+Daisy-Y-Cao@users.noreply.github.com>
2024-01-25 03:28:43 +0800

    Create sWAP cASE.py

---
+++ HackerRank Practices/sWAP cASE.py
@@ -0,0 +1,17 @@
+def swap_case(s):
+    new_s = ""
+    for letter in s:
+        if letter.isalpha():
+            if letter == letter.upper():
+                new_s = new_s + letter.lower()
+            else:
+                new_s = new_s + letter.upper()     
+        else:
+            new_s = new_s + letter
+    return new_s
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
